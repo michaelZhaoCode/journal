7039e4d4ccf1eb8fda7597ae86e8021162128fd2
michaelZhaoCode <82855036+michaelZhaoCode@users.noreply.github.com>
2022-01-02 10:14:39 -0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,9 +1,9 @@
 from os import listdir, remove
 from datetime import datetime
-
+# Change name format of files
 OLD_DATE_FORMAT = "%d-%m-%Y"
 NEW_DATE_FORMAT = "%Y-%m-%d"
-
+# Load current pages
 pages = [page for page in listdir("pages") if ".txt" in page]
 
 for page in pages:
@@ -11,8 +11,8 @@ for page in pages:
         text = file.read()
         dates = page[:-4].split("-")
         new_name = datetime.strptime(page[:-4], OLD_DATE_FORMAT).strftime(NEW_DATE_FORMAT) + ".txt"
-
+        # Create new file
         with open(f"pages/{new_name}", "w") as new_file:
             new_file.write(text)
-
+    # Remove old file
     remove(f"pages/{page}")
